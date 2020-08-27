89877497b961465626dad5aee90839e3b973c7a4
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-08-27 16:59:16 +0300

    ex.1-4

--- src/main/java/w1exercises/Algorithms.java
+++ src/main/java/w1exercises/Algorithms.java
@@ -1,15 +1,42 @@
 package w1exercises;
 
-import java.util.Scanner;
+
 
 public final class Algorithms {
 
-    public static final int factorial(int n){
+    public static int factorial(int n){
         if (n == 0)
             return 1;
         else
             return(n * factorial(n-1));
     }
 
+    public static int findMaxFact(){
+        for( int i = 1;; i++){
+            if(Integer.MAX_VALUE / factorial(i) < i){
+                return i-1;
+            }
+        }
+    }
 
+
+    public static boolean isPrime(int number) {
+        if (number<=1){
+            return false;
+        }
+        for(int i=2; i< number/2; i++){
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static double harmonicSeries(int n){
+        if (n==1){
+            return 1.0;
+        }
+        return (1.0/n)+harmonicSeries(n-1);
+
+    }
 }
--- src/main/java/w1exercises/Main.java
+++ src/main/java/w1exercises/Main.java
@@ -5,14 +5,46 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+//        Comment out the exercise you wish to run //
+
 //        1. Write a program that calculates the factorial of a number n
 //                (n!).
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please give an integer");
-        int n = scanner.nextInt();
-        System.out.println("the factorial of "+n+" is: "+Algorithms.factorial(n));
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please give an integer");
+//        int n = scanner.nextInt();
+//        scanner.close();
+//        System.out.println("the factorial of "+n+" is: "+Algorithms.factorial(n));
+
+
 //        2. Find the maximum value of n as int so that the calculated
 //        output is valid.
+
+//        System.out.println(Algorithms.findMaxFact()+" is the maximum integer for which its factorial is within the Integer limits");
+
+
+//        3. Write a program that returns true or false depending on if
+//        the number is prime or not
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please give an integer");
+//        int n = scanner.nextInt();
+//        scanner.close();
+//        System.out.println(Algorithms.isPrime(n) ? "That's a prime number" : "Not a prime number");
+
+
+//        4. Write a program that, for a given n, it calculates the
+//        following value: 1 + ½ + ⅓ + … + 1/n.
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please give a positive integer");
+//        int n = scanner.nextInt();
+//        scanner.close();
+//        System.out.println("1 + ½ + ⅓ + … + 1/n for n="+n+" gives "+Algorithms.harmonicSeries(n));
+
+
+//        5. Write a program that counts the digits of a long number.
+
 
     }
 
