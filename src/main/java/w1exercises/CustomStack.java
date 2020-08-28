8d6b7d3400a7c8625981b607215c86777ea4ea35
Thanos Tsianis <thanos95_ts@hotmail.com>
2020-08-28 03:55:01 +0300

    Custom Stack added

--- src/main/java/w1exercises/CustomStack.java
+++ src/main/java/w1exercises/CustomStack.java
@@ -2,8 +2,14 @@ package w1exercises;
 
 import java.util.Arrays;
 
-// We create out own implementation of a stack using an internal-hidden array
-// which expands its length if needed
+// We create out own implementation of a stack using an internal-hidden array of initial size 10,
+// that expands its length if needed
+// variable top is always equal to the size of the array-stack and 1 greater than the index of the element at the top of it
+// pop() returns the element at the top of the stack. In case of empty stack it returns null
+// push() insert the element at the top of the stack, but first it checks if there is available space
+// if not, then the expand() method is called which essentially replaces the hidden array with another one double its size
+
+
 
 public class CustomStack<T> {
 
